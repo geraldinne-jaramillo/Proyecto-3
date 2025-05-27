25d0e92d8ddfc45fb4ed031bdf410b12bdc5851f
jhincapie197 <jhincapie197@gmail.com>
2025-05-27 11:03:54 -0500

    feat creando el proyecto con variables de datos del equipo

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+       String nombre1 = "Geraldinne Jaramillo Avendaño";
+       int edad1 = 21;
+       int cedula1 = 1000443480;
+
+        String nombre2 = "Mateo Andrés Pineda Beltrán";
+        int edad2 = 19;
+        int cedula2 = 1066869266;
+
+        String nombre3 = "Edwin Smith Murillo Murillo";
+        int edad3 = 21;
+        int cedula3 = 1111668224;
+
+        String nombre4 = "Mariana Marin Vanegas";
+        int edad4 = 23;
+        int cedula4 = 1000440959;
+
+        String nombre5 = "Dilan Esteban Villada Mejia";
+        int edad5= 22;
+        int cedula5 = 1193565987;
+
+        String nombre6 = "Dariannys Daniela Zambrano";
+        int edad6 = 17;
+        int cedula6 = 1127923098;
+
+        String nombre7 = "Justin Londoño Cardenas";
+        int edad7 = 18;
+        int cedula7 = 1020416721;
+    }
+}
