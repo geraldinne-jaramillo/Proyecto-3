5a137da53edd43fd5471b4c72a77d9c93a141a48
jhincapie197 <jhincapie197@gmail.com>
2025-05-27 11:13:43 -0500

    refactor se agrega la va

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,29 +6,36 @@ public class Main {
        String nombre1 = "Geraldinne Jaramillo Avendaño";
        int edad1 = 21;
        int cedula1 = 1000443480;
+       String barrio1 = "Maruchenga";
 
         String nombre2 = "Mateo Andrés Pineda Beltrán";
         int edad2 = 19;
         int cedula2 = 1066869266;
+        String barrio2 = "Belén Rincón";
 
         String nombre3 = "Edwin Smith Murillo Murillo";
         int edad3 = 21;
         int cedula3 = 1111668224;
+        String barrio3 = "Enciso";
 
         String nombre4 = "Mariana Marin Vanegas";
         int edad4 = 23;
         int cedula4 = 1000440959;
+        String barrio4 = "Castilla";
 
         String nombre5 = "Dilan Esteban Villada Mejia";
         int edad5= 22;
         int cedula5 = 1193565987;
+        String barrio5 = "Maruchenga";
 
         String nombre6 = "Dariannys Daniela Zambrano";
         int edad6 = 17;
         int cedula6 = 1127923098;
+        String barrio6 = "Córdoba";
 
         String nombre7 = "Justin Londoño Cardenas";
         int edad7 = 18;
         int cedula7 = 1020416721;
+        String barrio7 = "Bello";
     }
 }
